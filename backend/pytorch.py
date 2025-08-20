404fa4618b03b135951937be1a33cf9d22f78923
leoho0722 <leo160918@gmail.com>
2025-08-21 02:07:57 +0800

    1. 新增使用 PyTorch 作為 backend 2. 新增 transformers、torch 等 dependencies

--- backend/pytorch.py
+++ backend/pytorch.py
@@ -1,17 +1,46 @@
 from typing import Any, Dict
 
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
 
 class PyTorchBackend:
-    def __init__(self):
-        pass
+    def __init__(self, model_id: str):
+        """Initialize the PyTorch backend.
 
-    async def generate(self, model: str, prompt: str, model_kwargs: Dict[str, Any]):
+        Args:
+            model_id (str): The model ID to load.
+        """
+
+        # Load model and tokenizer
+        self._load_model_and_tokenizer(model_id)
+
+    async def generate(self, prompt: str, model_kwargs: Dict[str, Any]):
         """LLM Inference using PyTorch as backend.
 
         Args:
-            model (str): The model to use for text generation.
             prompt (str): The prompt to use for text generation.
             model_kwargs (Dict[str, Any]): Additional model parameters.
         """
 
-        pass
+        pipe = pipeline(
+            "text-generation",
+            model=self.model,
+            tokenizer=self.tokenizer,
+        )
+
+        messages = [{"role": "user", "content": prompt}]
+
+        output = pipe(messages, max_tokens=model_kwargs.get("context_length", 8192))
+        print(output[0]["generated_text"][-1])
+
+    def _load_model_and_tokenizer(self, model_id: str):
+        """Load the model and tokenizer for text generation.
+
+        Args:
+            model_id (str): The model ID to load.
+        """
+
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_id, torch_dtype="auto", trust_remote_code=True, device_map="auto"
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
--- utils/prompt.py
+++ utils/prompt.py
@@ -1,3 +1,7 @@
+import json
+from typing import Any, Dict
+
+
 def read_prompt_file(file_path: str) -> str:
     """Read the prompt from a file.
 
@@ -17,3 +21,27 @@ def read_prompt_file(file_path: str) -> str:
     except Exception as e:
         print(f"An error occurred while reading the prompt file: {e}")
         return ""
+
+
+def read_prompt_json(file_path: str) -> Dict[str, Any]:
+    """Read the prompt from a JSON file.
+
+    Args:
+        file_path (str): The path to the prompt JSON file.
+
+    Returns:
+        content (Dict[str, Any]): The content of the prompt JSON file or an error message.
+    """
+
+    try:
+        with open(file_path, "r") as file:
+            return json.loads(file.read())
+    except FileNotFoundError:
+        print(f"Prompt JSON file '{file_path}' not found.")
+        return {"error": "File not found"}
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from file '{file_path}'.")
+        return {"error": "Invalid JSON"}
+    except Exception as e:
+        print(f"An error occurred while reading the prompt JSON file: {e}")
+        return {"error": "Unknown error"}
